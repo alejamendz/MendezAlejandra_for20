c19f7cf240fa4667038019240c7786c698a36c07
Alejandra <Alejandra@DESKTOP-HQKA5R3>
2026-02-24 19:26:50 -0600

    Programa Creado

--- MendezAlejandra_for20/Program.cs
+++ MendezAlejandra_for20/Program.cs
@@ -1,2 +1,2 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Pedir una cantidad N de números e indicar cuántos son positivos.");
