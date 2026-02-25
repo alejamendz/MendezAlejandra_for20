eceb3f89cc76c09f712f7725c58b3af16bc70898
Alejandra <Alejandra@DESKTOP-HQKA5R3>
2026-02-24 19:29:56 -0600

    Uso de for y fin del programa

--- MendezAlejandra_for20/Program.cs
+++ MendezAlejandra_for20/Program.cs
@@ -1,2 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Pedir una cantidad N de números e indicar cuántos son positivos.");
+
+Console.Write("¿Cuántos números?: ");
+int n = int.Parse(Console.ReadLine());
+int positivos = 0;
+
+for (int i = 1; i <= n; i++)
+{
+    Console.Write("Ingrese número: ");
+    int num = int.Parse(Console.ReadLine());
+
+    if (num > 0)
+        positivos++;
+}
+
+Console.WriteLine("Positivos: " + positivos);
